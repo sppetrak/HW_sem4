41e2f6d990844226bd6993efbce292014109feab
Petr <sppetrak@mail.ru>
2022-11-27 20:08:09 +0300

    maccbd

---
+++ task3/Program.cs
@@ -0,0 +1,13 @@
+﻿//Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран. 
+//(числа берете любые)
+
+int[] array = new int[8];
+
+Random arr = new Random();
+ 
+Console.WriteLine("Массив из 8 чисел в Рандоме");
+ for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(0, 100);
+   Console.Write("{0} ", array[i]);
+}
