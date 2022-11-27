c65c54949d1fed304e086778b7f12dcc5ae1093f
Petr <sppetrak@mail.ru>
2022-11-27 20:08:36 +0300

    массив

--- task3/Program.cs
+++ task3/Program.cs
@@ -6,7 +6,7 @@ int[] array = new int[8];
 Random arr = new Random();
  
 Console.WriteLine("Массив из 8 чисел в Рандоме");
- for (int i = 0; i < array.Length; i++)
+for (int i = 0; i < array.Length; i++)
 {
     array[i] = new Random().Next(0, 100);
    Console.Write("{0} ", array[i]);
